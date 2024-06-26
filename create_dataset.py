ce1de8340117474c09ef8c6eccafbea7585a962e
steve-story <51081535+steve-story@users.noreply.github.com>
2024-06-26 20:24:04 +0800

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -11,9 +11,10 @@ from skimage import img_as_float
 import cv2
 
 
-path = r"C:\Users\Administrator\Desktop\train\5\3"
-output_path = r'C:\Users\Administrator\Desktop\train\5\3png'
+path = r"C:\Users\Administrator\Desktop\train\2\20200113003910"  # kneedata is dataset's name
+output_path = r'./dataset/train'
 # path_2 = "./data/train/Data"
+
 
 def get_pixeldata(dicom_path):
     dataset = pydicom.dcmread(dicom_path)
@@ -25,6 +26,7 @@ def get_pixeldata(dicom_path):
     # 提取图像像素数据
     image = dataset.pixel_array.astype(float)
     image = cv2.resize(image, (224, 224))
+
     # 归一化
     image = (image - image.mean()) / image.std()
     print(image)
@@ -37,8 +39,8 @@ def dicom2png(dicom_path, savefile, width, height):
         os.makedirs(output_path)
 
     dcm = pydicom.dcmread(dicom_path)
-    print(dcm)
     # fileName = os.path.basename(file)
+
     imageX = dcm.pixel_array
     temp = imageX.copy()
     picMax = imageX.max()
@@ -48,23 +50,26 @@ def dicom2png(dicom_path, savefile, width, height):
     # print("vmax : ", vmax)
     imageX[imageX > vmax] = 0
     imageX[imageX < vmin] = 0
-    # result = exposure.is\_low\_contrast(imageX)
-    # # print(result)
     image = img_as_float(imageX)
+
     plt.cla()
-    plt.figure('adjust\_gamma', figsize=(width / 100, height / 100))
-    plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
+    # plt.figure('adjust\_gamma', figsize=(width / 100, height / 100))
+    # plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
     plt.imshow(image, 'gray')
     plt.axis('off')
     plt.savefig(savefile)
 
 
-names = os.listdir(path)
-print(names)
-for i in range(len(names)):
-    dicom_path = os.path.join(path, names[i])
-    png_name = os.path.splitext(names[i])[0]
-    dst_path = os.path.join(output_path, (png_name + '.png'))
-    pixel_array = get_pixeldata(dicom_path)
-    # dicom to png
-    dicom2png(dicom_path, dst_path, 256, 256)
+if __name__ == '__main__':
+    filenumber = os.listdir(path)
+    for i in range(len(filenumber)):
+        filepath = os.path.join(path, filenumber[i])
+        filenames = os.listdir(filepath)
+        for i in range(len(filenames)):
+            dicom_path = os.path.join(filepath, filenames[i])
+            print(dicom_path)
+            png_name = os.path.splitext(filenames[i])[0]
+            dst_path = os.path.join(output_path, (png_name + '.png'))
+            pixel_array = get_pixeldata(dicom_path)
+            # dicom to png
+            dicom2png(dicom_path, dst_path, 256, 256)
